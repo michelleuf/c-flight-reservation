efc126faed9366774a357371de11d1646c25d219
MichelleFdo <mufernando02@gmail.com>
2020-01-23 00:34:00 +0530

    complete get function with iss function

--- ass.cpp
+++ ass.cpp
@@ -1,36 +1,31 @@
 #include<iostream>
 #include <fstream>
 #include<string.h>
+#include <sstream>
 //#include<ass.h>
 #include<iomanip>
 #define max 50
-
 using namespace std;
 
 // functions may use --------------------
-void display_avail_flights();
-void view_a_flight(string flight_no);
 void file_read();
 int flight_count();
 
-
-//void view_flight();
-
-
-//---------------------------------------
+//variables may use ---------------------
 ifstream myfile;
 string line;
 int i;
 int fl_count;
 
-
+// structure of a given seat row --------
 struct row
 {
+	string row_no;
 	char type;
-	int seat_letter[6];
+	string seat_letters;
 };
 
-
+// flight class -------------------------
 class flight
 {
 	private:
@@ -42,36 +37,19 @@ class flight
 	
 	public:
 		void view_a_flight(string flight_no);
-		void file_read();
-
-		void getdata ()
-		{
-			//read the file line by line and get data---- file should convert to an array------******
-			flight_no=line;
-			getline(myfile,line);
-			date_time=line;
-			getline(myfile,line);
-			dept_airport=line;
-			getline(myfile,line);
-			arrive_airport=line;
-			
-			for (i=0;i<60;i++)		//if a row is missing it should be written as 0 in the seat_location array
-			{
-				getline(myfile,line);
-	//			if (seats)
-				seat_location[i].type=line[0];			//how this should be done
-			//	seat_location[i].seat_letter[0];
-			}
-			
-		}
+		void display_avail_flights();
+		void getdata();
 		
-
-
 };
+
+//creating the array for flight objects
 flight flarray[max];
 
 
-void file_read()
+
+// function set -----------------------------------------
+
+void file_read()		//read the file
 {
 	myfile.open("flights.txt");
 	if (myfile.is_open())
@@ -85,6 +63,69 @@ void file_read()
 		}
 	}
 }
+
+int flight_count()   //we can determine how much the array size is
+{
+	int fl_count=0;
+	myfile.open("flights.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile,line))
+		{
+			if (line.empty())
+			{
+				fl_count++;
+			}
+		}
+	}
+	myfile.close();
+	return fl_count;
+}
+
+void flight::getdata()
+		{
+			//read the file line by line and get data---- file should convert to an array------******
+			flight_no=line;
+			getline(myfile,line);
+			date_time=line;
+			getline(myfile,line);
+			dept_airport=line;
+			getline(myfile,line);
+			arrive_airport=line;
+			getline(myfile,line);
+			
+			istringstream iss(line);
+			for (i=0;i<60;i++)							//if a row is missing it should be written as 0 in the seat_location array
+			{
+				iss >>seat_location[i].row_no>>seat_location[i].type>>seat_location[i].seat_letters;
+        	}
+						
+		}
+		
+		
+
+void display_avail_flights()
+{
+	//read first line of each paragraph and detect the flight numbers
+	// then display it 
+	
+}
+void flight::view_a_flight(string flight_no)
+{
+	//find the corresponding data set
+	for (i=0;i<1000;i++)
+	{
+		if (flarray[i].flight_no==flight_no)
+		{
+			cout<<"total no of avail seats must display here";
+			cout<<flarray[i].dept_airport;
+			cout<<flarray[i].arrive_airport;
+			cout<<flarray[i].date_time;
+		}
+	}
+	cout<<endl;
+}
+
 
 int main()
 {
@@ -116,6 +157,9 @@ int main()
 		case 2: 
 			cout<<"enter the flight_no : ";
 			cin>>flight_no;
+			
+			flarray[1].getdata();
+			cout<<flarray[1].flight_no;
 			//if flight no match with avail flights
 				//	--------read the file
 				//then show avail seat numbers with classes 
@@ -145,41 +189,4 @@ int main()
 	
 	return 0;
 }
-int flight_count()   //we can determine how much the array size is
-{
-	int fl_count=0;
-	myfile.open("flights.txt");
-	if (myfile.is_open())
-	{
-		while (getline(myfile,line))
-		{
-			if (line.empty())
-			{
-				fl_count++;
-			}
-		}
-	}
-	myfile.close();
-	return fl_count;
-}
 
-void display_avail_flights()
-{
-	//read first line of each paragraph and detect the flight numbers
-	// then display it 
-}
-void flight::view_a_flight(string flight_no)
-{
-	//find the corresponding data set
-	for (i=0;i<1000;i++)
-	{
-		if (flarray[i].flight_no==flight_no)
-		{
-			cout<<"total no of avail seats must display here";
-			cout<<flarray[i].dept_airport;
-			cout<<flarray[i].arrive_airport;
-			cout<<flarray[i].date_time;
-		}
-	}
-	cout<<endl;
-}
---
+++ exxxxx.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string line= "5 b ABCD";
+    istringstream iss(line);
+
+    do
+    {
+        iss >> seat_location[i].row_no;
+        //seat_location[i].row_no << "Substring: " << subs << endl;
+    } while (line);
+}
